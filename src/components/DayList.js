2fd517e83357cd605c6bcec2b5c401f64efa30ab
Sszyh <zhuyer@outlook.com>
2022-10-06 16:58:16 -0700

    fix tiny problem of '<li>'

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,15 +4,14 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const days = props.days.map((day) => {
     return (
-      <li>
         <DayListItem
           key={day.id}
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
-          setDay={day.setDay}
+          setDay={props.setDay}
         />
-      </li>)
+      )
   });
   return (
     <ul>
