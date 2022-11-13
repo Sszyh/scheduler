5b2b220da4e3d320c6e514c94dbbe8f999ab07f8
Sszyh <zhuyer@outlook.com>
2022-11-13 15:25:10 -0800

    try useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,8 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
+import { act } from "@testing-library/react";
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+const CANCEL_INTERVIEW = "CANCEL_INTERVIEW";
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return {...state, day}
+    case SET_APPLICATION_DATA:
+      return {...state, days, appointments, interviewers}
+    case SET_INTERVIEW:
+      return {...state, interview}
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+// dispatch({ type: [SET_DAY], day })
+// dispatch({ type: [SET_APPLICATION_DATA], days, appointments, interviewers })
+// dispatch({ type: [SET_INTERVIEW], id, interview })
+// dispatch({ type: CANCEL_INTERVIEW, id, interview: null })
 
 export default function useApplicationData() {
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
@@ -59,7 +86,7 @@ export default function useApplicationData() {
       });
 
   }, []);
-  
+
   function setSpotsAdd(id) {
     if (id > 0 && id <= 5) {
       state.days[0].spots += 1;
