4a6853d8cc2e9388a74122ea68fed55c8f9c32c5
Sszyh <zhuyer@outlook.com>
2022-11-22 18:34:08 -0800

    clean up comments

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,16 +6,7 @@ import Application from "components/Application";
 
 import axios from "axios";
 afterEach(cleanup);
-/* Promise chain
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
-});
-*/
 describe("Application", () => {
 
   it("changes the schedule when a new day is selected", async () => {
@@ -34,9 +25,7 @@ describe("Application", () => {
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
-    /*
-    if do console.log(prettyDom(appoingments)); will display an <article> array
-    */
+    
     const appointment = appointments[0];
 
     fireEvent.click(getByAltText(appointment, "Add"));
@@ -47,9 +36,7 @@ describe("Application", () => {
     fireEvent.click(getByText(appointment, "Save"));
 
     expect(getByText(appointment, "Saving update")).toBeInTheDocument();
-    /* can use waitForElementToBeRemoved as following
-    await waitForElementToBeRemoved(() => getByText(appointment,"Saving update"));
-    */
+    
     await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
 
     const day = getAllByTestId(container, "day").find(day =>
@@ -60,15 +47,6 @@ describe("Application", () => {
   });
 
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-
-    // 1. Render the Application.
-    // 2. Wait until the text "Archie Cohen" is displayed.
-    // 3. Click the "Delete" button on the booked appointment.
-    // 4. Check that the confirmation message is shown.
-    // 5. Click the "Confirm" button on the confirmation.
-    // 6. Check that the element with the text "Deleting" is displayed.
-    // 7. Wait until the element with the "Add" button is displayed.
-    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
 
     const { container } = render(<Application />);
 
